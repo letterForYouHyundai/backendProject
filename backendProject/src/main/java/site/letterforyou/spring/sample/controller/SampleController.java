b36a18c2f9abacc2a9725beb37393f92c2e933fa
codeSweet0828 <codesweet0828@gmail.com>
2023-12-02 23:48:35 +0900

    Sample Controller Entity타입으로 변경

--- backendProject/src/main/java/site/letterforyou/spring/sample/controller/SampleController.java
+++ backendProject/src/main/java/site/letterforyou/spring/sample/controller/SampleController.java
@@ -1,27 +1,39 @@
 package site.letterforyou.spring.sample.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.java.Log;
 import site.letterforyou.spring.sample.domain.SampleVO;
 import site.letterforyou.spring.sample.service.SampleService;
+
+import java.util.ArrayList; // ArrayList import 추가
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/sample")
 @Log
 public class SampleController {
 
-	@Autowired
-	private SampleService sampleService;
-	
-	@RequestMapping("/test")
-	public void sampleTest(SampleVO svo) {
-	
-		List<SampleVO> list = sampleService.getSampleMemberList(svo);
-		log.info("샘플테스트");
-		log.info(list.toString());
-	}
+    @Autowired
+    private SampleService sampleService;
+
+    @RequestMapping("/test")
+    public ResponseEntity<Map<String, Object>> sampleTest(SampleVO svo) {
+    	
+    	Map <String, Object> map = new HashMap<String, Object>();
+    	
+        ArrayList<SampleVO> list = (ArrayList<SampleVO>) sampleService.getSampleMemberList(svo); //
+        map.put("list", list);
+        
+        //log.info("샘플테스트");
+    
+        return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+
+    }
 }
