29904f879353df38288365b54c017407f8572d4b
smartpodo@kakao.com <smartpodo@kakao.com>
2023-12-08 17:41:21 +0900

    add : board VOs

--- backendProject/src/main/java/site/letterforyou/spring/board/domain/CommentVO.java
+++ backendProject/src/main/java/site/letterforyou/spring/board/domain/CommentVO.java
@@ -3,12 +3,16 @@ package site.letterforyou.spring.board.domain;
 import java.time.LocalDateTime;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class CommentVO {
 
 	private Long commentId;
-	private Long board_no;
+	private Long boardNo;
 	private String userId;
 	private String commentContent;
 	private LocalDateTime registDate;
