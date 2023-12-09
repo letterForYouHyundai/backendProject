e1d19f60fd583a049e6372249c3d213e2a44531c
smartpodo@kakao.com <smartpodo@kakao.com>
2023-12-10 00:29:50 +0900

    delete : Member 관련 mapper 삭제

--- backendProject/src/main/java/site/letterforyou/spring/member/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package site.letterforyou.spring.member.mapper;
-
-import site.letterforyou.spring.member.domain.UserVO;
-
-public interface MemberMapper {
-	
-	public UserVO getUserByUserId(String userId);
-	
-}
