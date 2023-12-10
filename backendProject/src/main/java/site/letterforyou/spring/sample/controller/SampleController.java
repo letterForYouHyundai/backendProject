96cf0e82a3650188a6202af04748ed748b0a431a
smartpodo@kakao.com <smartpodo@kakao.com>
2023-12-10 19:12:31 +0900

    merge fix : dev -> board rebase 후 난 conflict 해결

--- backendProject/src/main/java/site/letterforyou/spring/sample/controller/SampleController.java
+++ backendProject/src/main/java/site/letterforyou/spring/sample/controller/SampleController.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.java.Log;
@@ -46,8 +44,7 @@ public class SampleController {
     }
     
     
-    	return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
-    }
+    	
     
     @GetMapping("/home")
     public ModelAndView testHome() {
