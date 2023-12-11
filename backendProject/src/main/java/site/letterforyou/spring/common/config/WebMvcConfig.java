9f3fc1be982c8d4fb32a0fe2d154046c5becd994
smartpodo@kakao.com <smartpodo@kakao.com>
2023-12-11 23:07:47 +0900

    test

---
+++ backendProject/src/main/java/site/letterforyou/spring/common/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package site.letterforyou.spring.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowedOrigins("*")
+                .allowedMethods("*");
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
