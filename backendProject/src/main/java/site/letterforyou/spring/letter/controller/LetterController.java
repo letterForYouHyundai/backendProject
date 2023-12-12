642556270c662b02234f0315f50901548e83bed7
smartpodo@kakao.com <smartpodo@kakao.com>
2023-12-13 01:52:50 +0900

    fix : letterNo 암호화 및 Session 적용

--- backendProject/src/main/java/site/letterforyou/spring/letter/controller/LetterController.java
+++ backendProject/src/main/java/site/letterforyou/spring/letter/controller/LetterController.java
@@ -64,48 +64,49 @@ public class LetterController {
 		Long defaultPage = 1L;
 
 		Long p = page == null ? defaultPage : page;
+	
 		String userId = sessionUtil.validSession(session);
 		log.info(": /letter/receive/list"+p);
-		// user 검증 추가
+		
 		return ResponseEntity.ok(letterService.getLetterReceiveList(p, userId));
 
 	}
 	
 	@GetMapping("/receive/{letterNo}")
 	public ResponseEntity<ResponseSuccessDTO<LetterGetLetterResponseDTO>> getReceivedLetter(
-			@PathVariable(value="letterNo") Long letterNo){
+			@PathVariable(value="letterNo") String letterNo){
 		return ResponseEntity.ok(letterService.getReceivedLetter(letterNo));
 	}
 	
 	@PutMapping("/receive/{letterNo}")
 	public ResponseEntity<ResponseSuccessDTO<LetterDeleteLetterResponseDTO>> deleteReceivedLetter(
-			@PathVariable(value="letterNo") Long letterNo){
+			@PathVariable(value="letterNo") String letterNo){
 		return ResponseEntity.ok(letterService.deleteReceivedLetter(letterNo));
 	}
 	
 	@GetMapping("/send/list")
 	public ResponseEntity<ResponseSuccessDTO<LetterGetListResponseDTO>> getSendLetters(
-			@RequestParam(value = "pageNo", required = false) Long page) {
+			@RequestParam(value = "pageNo", required = false) Long page, HttpSession session) {
 
 		Long defaultPage = 1L;
 
 		Long p = page == null ? defaultPage : page;
 
 		log.info(": /letter/send/list"+p);
-		// user 검증 추가
-		return ResponseEntity.ok(letterService.getLetterSendList(p, "user2"));
+		String userId = sessionUtil.validSession(session);
+		return ResponseEntity.ok(letterService.getLetterSendList(p, userId));
 
 	}
 	
 	@GetMapping("/send/{letterNo}")
 	public ResponseEntity<ResponseSuccessDTO<LetterGetLetterResponseDTO>> getSendLetter (
-			@PathVariable(value="letterNo") Long letterNo) throws Exception{
+			@PathVariable(value="letterNo") String letterNo){
 		return ResponseEntity.ok(letterService.getSendLetter(letterNo));
 	}
 	
 	@PutMapping("/send/{letterNo}")
 	public ResponseEntity<ResponseSuccessDTO<LetterDeleteLetterResponseDTO>> deleteSendLetter(
-			@PathVariable(value="letterNo") Long letterNo){
+			@PathVariable(value="letterNo") String letterNo){
 		return ResponseEntity.ok(letterService.deleteSendLetter(letterNo));
 	}
 }
