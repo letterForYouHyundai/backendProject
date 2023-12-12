d4c62a9eaa8bd4f1c39f62f671d95824191c2865
smartpodo@kakao.com <smartpodo@kakao.com>
2023-12-12 14:40:06 +0900

    fix : pagination 관심사 분리

--- backendProject/src/main/java/site/letterforyou/spring/template/controller/TemplateController.java
+++ backendProject/src/main/java/site/letterforyou/spring/template/controller/TemplateController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import site.letterforyou.spring.common.dto.PageRequestDTO;
 import site.letterforyou.spring.common.dto.ResponseSuccessDTO;
+import site.letterforyou.spring.common.util.PageUtil;
 import site.letterforyou.spring.template.dto.TemplateGetListResponseDTO;
 import site.letterforyou.spring.template.dto.TemplatePostLikeResponseDTO;
 import site.letterforyou.spring.template.service.TemplateService;
@@ -24,51 +26,46 @@ public class TemplateController {
 
 	@Autowired
 	private TemplateService templateService;
-	
-	
+
+	@Autowired
+	private PageUtil pageUtil;
+
 	@GetMapping("/list")
 	public ResponseEntity<ResponseSuccessDTO<TemplateGetListResponseDTO>> getTemplateList(
-			@RequestParam(value = "page" , required = false) Long page ,
-			@RequestParam(value= "sortBy" ,required = false) String sortBy, 
-			@RequestParam(value= "inOrder" ,required = false) Integer inOrder) {
+			@RequestParam(value = "page", required = false) Long page,
+			@RequestParam(value = "sortBy", required = false) String sortBy,
+			@RequestParam(value = "inOrder", required = false) Integer inOrder) {
 
-	    Long defaultPage = 1L;
-	    String defaultSortBy="likes";
-	    int defaultInOrder =1;
-	    Long p  = page == null ? defaultPage :page;
-	    String sb = sortBy == null? defaultSortBy : sortBy;
-	    int io = inOrder == null ? defaultInOrder :inOrder;
-	    log.info(": /template/list/" + p);
-	    
-	    return ResponseEntity.ok(templateService.getTemplateList(sb, io, p));
+		PageRequestDTO pageDTO = pageUtil.parsePaginationComponents(page, sortBy, inOrder);
+
+		log.info(": /template/list/" + page);
+
+		return ResponseEntity
+				.ok(templateService.getTemplateList(pageDTO.getSortBy(), pageDTO.getInOrder(), pageDTO.getPage()));
 	}
-	
-	// 제목 으로 검색 
+
+	// 제목 으로 검색
 	@GetMapping("/search")
 	public ResponseEntity<ResponseSuccessDTO<TemplateGetListResponseDTO>> getTemplateSearch(
-			@RequestParam(value = "keyword" , required = false) String keyword,
-			@RequestParam(value = "page" , required = false) Long page ,
-			@RequestParam(value= "sortBy" ,required = false) String sortBy, 
-			@RequestParam(value= "inOrder" ,required = false) Integer inOrder) {
+			@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "page", required = false) Long page,
+			@RequestParam(value = "sortBy", required = false) String sortBy,
+			@RequestParam(value = "inOrder", required = false) Integer inOrder) {
 
+		PageRequestDTO pageDTO = pageUtil.parsePaginationComponents(page, sortBy, inOrder);
 		String defaultKeyword = "empty";
-	    Long defaultPage = 1L;
-	    String defaultSortBy="likes";
-	    int defaultInOrder =1;
-	    Long p  = page == null ? defaultPage :page;
-	    String sb = sortBy == null? defaultSortBy : sortBy;
-	    int io = inOrder == null ? defaultInOrder :inOrder;
-	    String k = keyword == null ? defaultKeyword : keyword;
-	    log.info(": /template/list/" + p);
-	    
-	    return ResponseEntity.ok(templateService.getTemplateSearch(k, sb, io, p));
+		String k = keyword == null ? defaultKeyword : keyword;
+		log.info(": /template/list/" + pageDTO.getPage());
+
+		return ResponseEntity
+				.ok(templateService.getTemplateSearch(k, pageDTO.getSortBy(), pageDTO.getInOrder(), pageDTO.getPage()));
 	}
-	
+
 	@PostMapping("/likes/{templateNo}")
 	public ResponseEntity<ResponseSuccessDTO<TemplatePostLikeResponseDTO>> postTemplateLike(
-			@PathVariable(value="templateNo") Long templateNo , HttpSession session) {
-			//String userId = session.getAttribute("");
-	    	String userId = "2";
-	    return ResponseEntity.ok(templateService.postTemplateLike(templateNo ,userId));
+			@PathVariable(value = "templateNo") Long templateNo, HttpSession session) {
+		// String userId = session.getAttribute("");
+		String userId = "2";
+		return ResponseEntity.ok(templateService.postTemplateLike(templateNo, userId));
 	}
 }
